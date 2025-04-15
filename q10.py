1999d621ae88e167f0ab7b1b050297a0da1ac03b
Ruan-Carlos-dev <ruancarlosdejesus0@gmail.com>
2025-04-15 20:37:58 -0300

    Respostas da Q10

--- q10.py
+++ q10.py
@@ -1,1 +1,8 @@
+salario_bruto = 5000
+desconto_inss =  salario_bruto * 0.15
+desconto_ir = salario_bruto * 0.10
+imposto = desconto_inss + desconto_ir
 
+
+salario_liquido = salario_bruto - imposto
+print(salario_liquido)
